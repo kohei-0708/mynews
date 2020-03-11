3a688cdf036b3f3f24da8ce9985cdbefd75fdb5b
yasuda kohei <ko.20112135@gmail.com>
2020-03-11 17:21:04 +0000

    課題追加

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyNews</title>
+        <title>MyProfile</title>
     </head>
     <body>
-        <h1>課題３CREATE</h1>
+        <h1>課題3CREATE</h1>
     </body>
 </html>
